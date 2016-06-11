a0f98eb303f196e28872d0c4c08fdf1ac6b51bcb
Mohsen Malmir <rubi@mohsens-mac-pro.dynamic.ucsd.edu>
2016-06-10 17:19:16 -0700

    init files added

--- DeepLearningStack/ConvNet/DepthConcat.py
+++ DeepLearningStack/ConvNet/DepthConcat.py
@@ -10,8 +10,8 @@ import theano.tensor as T
 
 # implementing flatten
 class DepthConcat(object):
-	""" Initialize from xml definition node """
-	def __init__(self,layer_def,inputs,inputs_shape,rs,clone_from=None):
+    """ Initialize from xml definition node """
+    def __init__(self,layer_def,inputs,inputs_shape,rs,clone_from=None):
         """
             Create a depth concatenation layer. This is for concatenating outputs of convolutional layers, for example to create inception modules 
             The output is created by concatenating the inputs along dimension 0
@@ -24,19 +24,19 @@ class DepthConcat(object):
 
             :type rng: a random number generator used to initialize weights
         """
-		self.inputs        = inputs
-		n_channels 		   = np.sum([sz[0] for sz in inputs_shape])
-		batch_size         = inputs_shape[0][3]
-		dim1			   = inputs_shape[0][1]
-		dim2               = inputs_shape[0][2]
-		for sz in inputs_shape:
-			assert(dim1       == sz[1])
-			assert(dim2       == sz[2])
-			assert(batch_size == sz[3])
-		self.output        = T.concatenate(inputs,axis=0)
-		self.input_shape   = inputs_shape
-		self.output_shape  = [n_channels,dim1,dim2,batch_size]
-		self.params        = []
+        self.inputs        = inputs
+        n_channels         = np.sum([sz[0] for sz in inputs_shape])
+        batch_size         = inputs_shape[0][3]
+        dim1               = inputs_shape[0][1]
+        dim2               = inputs_shape[0][2]
+        for sz in inputs_shape:
+            assert(dim1       == sz[1])
+            assert(dim2       == sz[2])
+            assert(batch_size == sz[3])
+        self.output        = T.concatenate(inputs,axis=0)
+        self.input_shape   = inputs_shape
+        self.output_shape  = [n_channels,dim1,dim2,batch_size]
+        self.params        = []
 
 
 
